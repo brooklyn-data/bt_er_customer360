ae9c5ada8bd8c8f8dba205aa6c3dd3b8d4135667
garyavalosc <47262367+garyavalosc@users.noreply.github.com>
2024-03-20 14:32:57 -0600

    Update int_outerjoin_intacct_revenue.sql

--- models/intermediate/customer360/entityresolution/int_outerjoin_intacct_revenue.sql
+++ models/intermediate/customer360/entityresolution/int_outerjoin_intacct_revenue.sql
@@ -1,7 +1,14 @@
+{{ config(
+    materialized="table",
+    database="customer360",
+    schema="intermediate",
+
+) }}
+
 with
     source_intacct_rev as (
         select
-            source_id
+            r_num
             , csr_user_name
         from {{ ref('int_customers_unioned') }}
         where source = 'intacct revenue'
@@ -10,18 +17,18 @@ with
     customers_unioned as (
         select
             source
-            , source_id
-            , dup_imp_proj_source_id
-            , dup_product_usg_source_id
-            , dup_sf_opp_source_id
+            , r_num
+            , dup_imp_proj_r_num
+            , dup_product_usg_r_num
+            , dup_sf_opp_r_num
             , salesforce_account_id
-            , salesforce_opportunity_id
-            , project_id
+            --, salesforce_opportunity_id
+            --, project_id
             , billing_id
-            , billing_platform_account_id
+            --, billing_platform_account_id
             , csr_user_name
             , csr_user_id
-            , tenant_id
+            --, tenant_id
         from {{ ref('int_outerjoin_sf_opp') }} where 
         source != 'intacct revenue'
     )
@@ -29,19 +36,19 @@ with
 
 select
     a.source
-    , a.source_id
-    , dup_imp_proj_source_id
-    , dup_product_usg_source_id
-    , dup_sf_opp_source_id
-    , b.source_id as dup_intacct_revenue_source_id
+    , a.r_num
+    , dup_imp_proj_r_num
+    , dup_product_usg_r_num
+    , dup_sf_opp_r_num
+    , b.r_num as dup_intacct_revenue_r_num
     , a.salesforce_account_id
-    , a.salesforce_opportunity_id
-    , a.project_id
+   --, a.salesforce_opportunity_id
+    --, a.project_id
     , a.billing_id
-    , a.billing_platform_account_id
+    --, a.billing_platform_account_id
    , coalesce(a.csr_user_name, b.csr_user_name ) as  csr_user_name
     , a.csr_user_id
-    , a.tenant_id
+    --, a.tenant_id
 from customers_unioned as a
 full outer join
     source_intacct_rev as b on
