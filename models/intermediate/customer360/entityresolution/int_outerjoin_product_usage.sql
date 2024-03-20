727a00b9c7ef5306ed11473500607b461a3f6890
garyavalosc <47262367+garyavalosc@users.noreply.github.com>
2024-03-20 14:31:45 -0600

    Update int_outerjoin_product_usage.sql

--- models/intermediate/customer360/entityresolution/int_outerjoin_product_usage.sql
+++ models/intermediate/customer360/entityresolution/int_outerjoin_product_usage.sql
@@ -6,22 +6,21 @@
 ) }}
 
 with
-    source_product_usage as (
+    source_sf_opp as (
         select
             r_num
             , salesforce_account_id
-            , csr_user_name
-            , billing_id
+            --, salesforce_opportunity_id
         from {{ ref('int_customers_unioned') }}
-        where source = 'product usage'
+        where source = 'salesforce opportunities'
         and coalesce(salesforce_account_id,'') != ''
-        and coalesce(billing_id,'') != ''
     ),
     customers_unioned as (
         select
             source
             , r_num
             , dup_imp_proj_r_num
+            , dup_product_usg_r_num
             , salesforce_account_id
             --, salesforce_opportunity_id
             --, project_id
@@ -30,8 +29,8 @@ with
             , csr_user_name
             , csr_user_id
             --, tenant_id
-        from {{ ref('int_outerjoin_implementation_project') }} where
-        source != 'product usage'
+        from {{ ref('int_outerjoin_product_usage') }} where 
+        source != 'salesforce opportunities'
     )
 
 
@@ -39,17 +38,17 @@ select
     a.source
     , a.r_num
     , dup_imp_proj_r_num
-    , b.r_num as dup_product_usg_r_num
+    , dup_product_usg_r_num
+    , b.r_num as dup_sf_opp_r_num
     , coalesce(a.salesforce_account_id, b.salesforce_account_id) as salesforce_account_id
-    --, a.salesforce_opportunity_id
+   -- , coalesce(a.salesforce_opportunity_id, b.salesforce_opportunity_id ) as  salesforce_opportunity_id
     --, a.project_id
-    , coalesce(a.billing_id, b.billing_id) as billing_id
+    , a.billing_id
     --, a.billing_platform_account_id
-    , coalesce(a.csr_user_name, b.csr_user_name ) as  csr_user_name
+    , a.csr_user_name
     , a.csr_user_id
     --, a.tenant_id
 from customers_unioned as a
 full outer join
-    source_product_usage as b on
+    source_sf_opp as b on
     a.salesforce_account_id = b.salesforce_account_id
-    and a.billing_id = b.billing_id
