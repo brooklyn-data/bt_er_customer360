1f0f3119117b8b6e56210409cc0c7de06cff32cc
garyavalosc <47262367+garyavalosc@users.noreply.github.com>
2024-03-20 14:29:54 -0600

    Update int_outerjoin_implementation_project.sql

--- models/intermediate/customer360/entityresolution/int_outerjoin_implementation_project.sql
+++ models/intermediate/customer360/entityresolution/int_outerjoin_implementation_project.sql
@@ -1,9 +1,16 @@
+{{ config(
+    materialized="table",
+    database="customer360",
+    schema="intermediate"
+
+) }}
+
 with
     source_implementation_projects as (
         select
-            source_id
+            r_num
             , salesforce_account_id
-            , salesforce_opportunity_id
+            --, salesforce_opportunity_id
             , billing_id
         from {{ ref('int_customers_unioned') }}
         where source = 'implementation projects'
@@ -13,34 +20,35 @@ with
     customers_unioned as (
         select
             source
-            , source_id
+            , r_num
             , salesforce_account_id
-            , salesforce_opportunity_id
-            , project_id
+            --, salesforce_opportunity_id
+            --, project_id
             , billing_id
-            , billing_platform_account_id
+            --, billing_platform_account_id
             , csr_user_name
             , csr_user_id
-            , tenant_id
+            --, tenant_id
         from {{ ref('int_customers_unioned') }}
         where source != 'implementation projects'
-    )
+    ),
 
-
-select
+final as (select
     a.source
-    , a.source_id
-    , b.source_id as dup_imp_proj_source_id
-    , a.salesforce_account_id
-    , coalesce(a.salesforce_opportunity_id, b.salesforce_opportunity_id ) as  salesforce_opportunity_id
-    , a.project_id
-    , a.billing_id
-    , a.billing_platform_account_id
+    , a.r_num
+    , b.r_num as dup_imp_proj_r_num
+    , coalesce(a.salesforce_account_id, b.salesforce_account_id) as salesforce_account_id
+    --, coalesce(a.salesforce_opportunity_id, b.salesforce_opportunity_id ) as  salesforce_opportunity_id
+    --, a.project_id
+    , coalesce(a.billing_id, b.billing_id) as billing_id
+    --, a.billing_platform_account_id
     , a.csr_user_name
     , a.csr_user_id
-    , a.tenant_id
+    --, a.tenant_id
 from customers_unioned as a
-full outer join
+full outer join 
     source_implementation_projects as b on
     a.salesforce_account_id = b.salesforce_account_id
-    and a.billing_id = b.billing_id
+    and a.billing_id = b.billing_id)
+
+select * from final
